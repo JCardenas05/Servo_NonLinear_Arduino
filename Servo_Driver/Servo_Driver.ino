7c3cb3e4d0a752df3a31b5924f3ddaf409342551
JCardenas05 <jocardenash@unal.edu.co>
2024-08-10 23:43:41 -0500

    Code Asíncrono V2.0

--- Servo_Driver/Servo_Driver.ino
+++ Servo_Driver/Servo_Driver.ino
@@ -3,51 +3,61 @@
 class MiServo : public Servo {
   private: 
     unsigned long last_time = 0;
-    int angle_no_linear = 5;
-    int velocity_slow = 150;
+    int angle_no_linear = 10;
+    int velocity_slow = 300;
 
     bool motion_in_progress = false;
     bool direction = true;
     int slow_start_angle = 0;
     int current_angle = 0;
+    int end_angle = 0;
 
   public:
     void NonlinearMotion(int angle) {
         current_angle = read();
-        if (current_angle != angle) {
-          direction = true ? current_angle < angle : false;
+        end_angle = angle;
+        if (current_angle != end_angle) {
+          direction = true ? current_angle < end_angle : false;
           if (direction){
-            slow_start_angle = max(current_angle, angle - angle_no_linear);
+            slow_start_angle = max(current_angle, end_angle - angle_no_linear);
           }
           else {
-            slow_start_angle = min(current_angle, angle + angle_no_linear);
+            slow_start_angle = min(current_angle, end_angle + angle_no_linear);
           }
           motion_in_progress = true;
+          write(slow_start_angle);
+          current_angle = slow_start_angle;
         }
         else {
+          motion_in_progress = false;
           return;
-        }
-
-        write(slow_start_angle);
-
-        if (direction) {
-          for (int i = slow_start_angle; i <= angle; i++) {
-            write(i);
-            delay(velocity_slow); 
-          }
-        }
-        else {
-          for (int i = slow_start_angle; i >= angle; i--) {
-            write(i);
-            delay(velocity_slow); 
-          }
         }
       }
     
     void update() {
-      if (millis() - last_time > 1000) {
+      unsigned long current_time = millis();
+      if (motion_in_progress & current_time - last_time > velocity_slow) {
         last_time = millis();
-        Serial.println("Hola");
+        if (direction) {
+          if (current_angle < end_angle) {
+            current_angle++;
+            write(current_angle);
+          }
+          else {
+            motion_in_progress = false;
+            Serial.println("motion finished");
+          }
+        }
+        else {
+          if (current_angle > end_angle) {
+            current_angle--;
+            write(current_angle);
+          }
+          else {
+            motion_in_progress = false;
+            Serial.println("motion inverse finished");
+          }
+        }
       }
     }
 
@@ -61,21 +71,28 @@ class MiServo : public Servo {
 
 MiServo miServo1;
 MiServo miServo2;
+MiServo miServo3;
 
 void setup() {
   Serial.begin(9600);
   miServo1.attach(10);
   miServo2.attach(11);
+  miServo3.attach(9);
   miServo1.Origin();
   miServo2.Origin();
+  miServo3.Origin();
   delay(3000);
   miServo1.Midle();
   miServo2.Midle();
   delay(3000);
-  miServo1.NonlinearMotion(50);
-  miServo2.NonlinearMotion(130);
+  miServo1.NonlinearMotion(25);
+  miServo2.NonlinearMotion(95);
+  miServo3.NonlinearMotion(180);
 }
 
 void loop() {
   // Tu código aquí
+  miServo1.update();
+  miServo2.update();
+  miServo3.update();
 }
